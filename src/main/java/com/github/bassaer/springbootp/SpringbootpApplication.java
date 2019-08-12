d1ec88ca2bd5554c6d47bbbd668d52e91396b38e
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-08-12 20:28:00 +0900

    init

---
+++ src/main/java/com/github/bassaer/springbootp/SpringbootpApplication.java
@@ -0,0 +1,13 @@
+package com.github.bassaer.springbootp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/bassaer/springbootp/SpringbootpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.bassaer.springbootp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
